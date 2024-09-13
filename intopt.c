437494458c7cbdf09f4a83d77d753ad80aef2343
Simon Alveteg <simon.alveteg@gmail.com>
2024-09-13 09:48:11 +0200

    Lab 1 Done.

--- intopt.c
+++ intopt.c
@@ -7,41 +7,43 @@ double** make_matrix(int m, int n) {
 
   a = calloc(m, sizeof(double*));
   for (int i = 0; i < m; i += 1) {
-    a[i] = calloc(n-1, sizeof(double));
+    a[i] = calloc(n, sizeof(double));
   }
   return a;
 }
 
 int main(int argc, char *argv[])
 {
-  int a;
-  int b;
-  double** c;
+  int m;
+  int n;
+  double** a;
+  double b; 
+  double c1;
+  double c2;
   
-  scanf("%d %d", &a, &b);
-  c = make_matrix(a, b);
+  scanf("%d %d", &m, &n);
+  scanf("%lf %lf", &c1, &c2);
+  a = make_matrix(m, n);
 
-  for (int i = 0; i < a; i += 1) {
-    for (int j = 0; j < b; j += 1) {
-      scanf("%lf", &c[i][j]);
+  for (int i = 0; i < m; i += 1) {
+    for (int j = 0; j < n; j += 1) {
+      scanf("%lf", &a[i][j]);
     }
   }
-  for (int i = 0; i < a - 1; i++) {
-    if (i == 0) {
-      printf("max z = ");
-      for (int j = 0; j < b; j++) {
-        printf("%+5.1lfx%d", c[i][j], j);
-      }
-      printf("\n");
-      continue;
+  printf("max z = %5.1lfx0 %+5.1lfx1\n", c1, c2);
+  for (int i = 0; i < m; i++) {
+    /*printf("%+10.3lfx0 %+10.3lfx1 <= %10.3lf\n", a[i][0], a[i][1], a[m-1][i]);*/
+    for (int j = 0; j < n; j++) {
+      printf("%+10.3lfx%d", a[i][j], j);
     }
-    for (int j = 0; j < b; j++) {
-      printf("%+10.3lfx%d", c[i][j], j);
-    }
-    printf("<=%10.3lf", c[a-1][i-1]);
-    printf("\n");
+    scanf("%lf", &b);
+    printf("<=%10.3lf\n", b);
   }
-  free(c);
+
+ for (int i = 0; i < m; i += 1) {
+    free(a[i]);
+ }
+ free(a);
 
   return 0;
 }
