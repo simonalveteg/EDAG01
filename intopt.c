6f88aba2ba15749e0f21c0b991a9ac13caa39c59
Simon Alveteg <simon.alveteg@gmail.com>
2024-09-17 11:41:44 +0200

    init done

--- intopt.c
+++ intopt.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void* xmalloc(size_t size) {
+  void* ptr = malloc(size);
+
+  if (ptr == NULL) {
+    fprintf(stderr, "out of memory\n");
+    exit(1);
+  }
+
+  return ptr;
+}
+
+typedef struct simplex_t {
+  int m;
+  int n;
+  int *var;
+  double **a;
+  double *b;
+  double *x;
+  double *c;
+  double y;
+} simplex_t;
+
+int init(simplex_t *s, int m, int n, double** a, double* b, double* c, double* x, double y, int* var) {
+  int i, k;
+  s->m = m;
+  s->n = n;
+  s->a = a;
+  s->b = b;
+  s->c = c;
+  s->x = x;
+  s->y = y;
+  s->var = var;
+
+  if (s->var == NULL) {
+    s->var = xmalloc((m+n)*sizeof(int));
+    for (i = 0; i < m+n; i++) {
+      s->var[i] = i;
+    }
+  }
+  for (k = 0; i = 1; i++) {
+    if (b[i] < b[k]) {
+      k = i;
+    }
+  }
+  return k;
+}
+
 double** make_matrix(int m, int n) {
   double** a;
   int i;
