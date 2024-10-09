ba32688a1243608501baeed2bca35339c9cc1a15
simonalveteg <simon.alveteg@gmail.com>
2024-10-09 15:16:56 +0200

    move header to main file

--- intopt.c
+++ intopt.c
@@ -1,7 +1,40 @@
-#include "intopt.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h> // for NAN
+
+// header
+
+typedef struct simplex_t {
+  int m;
+  int n;
+  int *var;
+  double **a;
+  double *b;
+  double *x;
+  double *c;
+  double y;
+} simplex_t;
+
+int init(simplex_t *s, int m, int n, double** a, double* b, double* c, double* x, double y, int* var);
+
+void pivot(simplex_t *s, int row, int col);
+
+void prepare(simplex_t *s, int k);
+
+int initial(simplex_t *s, int m, int n, double** a, double* b, double* c, double* x, double y, int* var);
+
+int select_nonbasic(simplex_t *s);
+
+double xsimplex(int m, int n, double** a, double* b, double* c, double* x, double y, int* var, int h);
+
+double simplex(int m, int n, double** a, double* b, double* c, double* x, double y);
+
+double** make_matrix(int m, int n);
+
+int main(int argc, char *argv[]);
+
+
+// end of header
 
 const double epsilon = 1e-9;
 
